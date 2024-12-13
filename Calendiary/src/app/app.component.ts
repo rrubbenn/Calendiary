b81a3b234aa9860e7a8153a4d1ad41cec416482c
rrubbenn <rusanchop@gmail.com>
2024-12-13 09:14:08 +0100

    feat(layout): added sidebar menu

--- Calendiary/src/app/app.component.ts
+++ Calendiary/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from "./shared/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
