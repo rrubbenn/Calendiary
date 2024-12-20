1ba8c0f863f801d36c23c44d25be744a8ddecb86
rrubbenn <rusanchop@gmail.com>
2024-12-20 15:21:18 +0100

    feat(structure): added tasks & reorganized colours
    List of tasks added
    Dropdown (edit, delete options)
    OnClick reveal description of task

--- Calendiary/src/app/dashboard/dashboard.component.ts
+++ Calendiary/src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,64 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../shared/menu/menu.component";
-import { HeaderComponent } from "../shared/header/header.component";
 import { MatIconModule } from '@angular/material/icon';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { NgChartsModule } from 'ng2-charts';
+import { NgStyle } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenuComponent, HeaderComponent, MatIconModule, NgChartsModule],
+  imports: [MenuComponent, MatIconModule, MatMenuModule, NgChartsModule, NgStyle],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  tasks = [
+    {
+      id: 1,
+      group: 'Development',
+      name: 'Analysis Task',
+      description: 'Analyze the project requirements.',
+      date: '2024-12-01',
+      priority: 'High'
+    },
+    {
+      id: 2,
+      group: 'Design',
+      name: 'Design User Interface',
+      description: 'Create UI prototypes for the project.',
+      date: '2024-12-03',
+      priority: 'Moderate'
+    },
+    {
+      id: 3,
+      group: 'Testing',
+      name: 'Integration Testing',
+      description: 'Perform integration tests with the system.',
+      date: '2024-12-10',
+      priority: 'Low'
+    },
+    {
+      id: 4,
+      group: 'Development',
+      name: 'Backend Coding',
+      description: 'Develop the API for the application.',
+      date: '2024-12-15',
+      priority: 'High'
+    },
+    {
+      id: 5,
+      group: 'Documentation',
+      name: 'Document Code',
+      description: 'Write documentation for the backend code.',
+      date: '2024-12-20',
+      priority: 'Moderate'
+    }
+  ];
 
   public charts = [
     {
@@ -69,15 +115,41 @@ export class DashboardComponent {
     events: [],
   };
 
-  visibleTable = false;
+  visibleDescription = false;
 
   calculatePercentage (AllTasks: number, Tasks: number) {
     return Math.floor((AllTasks / Tasks) * 100) + "%";
   }
 
-  showTable() {
-    this.visibleTable = !this.visibleTable;
-    
+  getColor(priority: string) {
+    switch(priority) {
+      case 'High':
+        return 'var(--high-priority)'
+      case 'Moderate':
+        return 'var(--moderate-priority)'
+      case 'Low':
+        return 'var(--low-priority)'
+      default:
+        return ''
+    }
+  }
+
+  visibleTaskId: number | null = null;
+
+  showDescription(taskId: number) {
+    if (this.visibleTaskId === taskId) {
+      this.visibleTaskId = null;
+    } else {
+      this.visibleTaskId = taskId;
+    }
+  }
+
+  editTask(taskId: number) {
+    console.log(taskId)
+  }
+
+  deleteTask(taskId: number) {
+    console.log(taskId)
   }
 
 }
