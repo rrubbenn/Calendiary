6db3ba236c611f4cfb7c454b9d50bce2f48f95f4
rrubbenn <rusanchop@gmail.com>
2024-12-20 08:48:17 +0100

    feat(structure): added basic routerlink
    Added basic router link from home to dashboard

--- Calendiary/src/app/app.component.ts
+++ Calendiary/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./shared/header/header.component";
-import { FooterComponent } from './shared/footer/footer.component';
-import { MenuComponent } from "./shared/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Calendiary/src/app/app.routes.ts
+++ Calendiary/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+export const routes: Routes = [
+    {
+        path: '', // <your-domain>/
+        component: HomeComponent,
+        title: 'No task selected'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: 'Dashboard'
+    }
+]
